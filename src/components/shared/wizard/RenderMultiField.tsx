0924b69e76715b0188cc8ada38a2e66daddc9798
Arnei <arnewilken@yahoo.de>
2025-03-11 09:10:58 +0100

    Attempt at rewriting multifield
    There are multiple elements that should
    be showing depending on focus, which
    causes all kinds of bad side effects.
    For example, blue button gets unmounted
    before the onclick handler triggers.

--- src/components/shared/wizard/RenderMultiField.tsx
+++ src/components/shared/wizard/RenderMultiField.tsx
@@ -23,101 +23,112 @@ const RenderMultiField = ({
 	form: FieldProps["form"]
 	showCheck?: boolean,
 }) => {
-	// Indicator if currently edit mode is activated
-	const {editMode, setEditMode} = useClickOutsideField(childRef);
-	// Temporary storage for value user currently types in
-	const [inputValue, setInputValue] = useState("");
+	// const { t } = useTranslation();
 
-	let fieldValue = [...field.value];
+	// // Indicator if currently edit mode is activated
+	// const {editMode, setEditMode} = useClickOutsideField(childRef);
+	// // Temporary storage for value user currently types in
+	// const [inputValue, setInputValue] = useState("");
 
-	// Handle change of value user currently types in
-	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-		const itemValue = e.target.value;
-		setInputValue(itemValue);
-	};
+	// let fieldValue = [...field.value];
 
-	const handleKeyDown = (event: React.KeyboardEvent) => {
-		// Check if pressed key is Enter
-		if (event.keyCode === 13 && inputValue !== "") {
-			event.preventDefault();
+	// // Handle change of value user currently types in
+	// const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+	// 	const itemValue = e.target.value;
+	// 	setInputValue(itemValue);
+	// };
 
-			submitValue();
-		}
-	};
+	// const handleKeyDown = (event: React.KeyboardEvent) => {
+	// 	// Check if pressed key is Enter
+	// 	if (event.keyCode === 13 && inputValue !== "") {
+	// 		event.preventDefault();
 
-	const submitValue = (alternativeInput?: string) => {
+	// 		submitValue();
+	// 	}
+	// };
 
-		let newInputValue = inputValue
-		if (alternativeInput) {
-			newInputValue = alternativeInput
-		}
+	// const submitValue = (alternativeInput?: string) => {
 
-		if (newInputValue !== "") {
-			let splitArray = [];
-			if (fieldInfo.delimiter) {
-				splitArray = newInputValue.split(fieldInfo.delimiter).map(item => item.trim()).filter(Boolean);
-			} else {
-				splitArray = [newInputValue];
-			}
+	// 	let newInputValue = inputValue
+	// 	if (alternativeInput) {
+	// 		newInputValue = alternativeInput
+	// 	}
 
-			for (const newInput of splitArray) {
-				// Flag if only values of collection are allowed or any value
-				if (onlyCollectionValues) {
-					// add input to formik field value if not already added and input in collection of possible values
-					if (
-						!fieldValue.find((e) => e === newInput) &&
-						fieldInfo.collection?.find((e) => e.value === newInput)
-					) {
-						fieldValue[fieldValue.length] = newInput;
-						form.setFieldValue(field.name, fieldValue);
-					}
-				} else {
-					// add input to formik field value if not already added
-					if (!fieldValue.find((e) => e === newInput)) {
-						fieldValue[fieldValue.length] = newInput;
-						form.setFieldValue(field.name, fieldValue);
-					}
-			}
-		}
+	// 	if (newInputValue !== "") {
+	// 		let splitArray = [];
+	// 		if (fieldInfo.delimiter) {
+	// 			splitArray = newInputValue.split(fieldInfo.delimiter).map(item => item.trim()).filter(Boolean);
+	// 		} else {
+	// 			splitArray = [newInputValue];
+	// 		}
 
-			// reset inputValue
-			setInputValue("");
-		}
-	}
+	// 		for (const newInput of splitArray) {
+	// 			// Flag if only values of collection are allowed or any value
+	// 			if (onlyCollectionValues) {
+	// 				// add input to formik field value if not already added and input in collection of possible values
+	// 				if (
+	// 					!fieldValue.find((e) => e === newInput) &&
+	// 					fieldInfo.collection?.find((e) => e.value === newInput)
+	// 				) {
+	// 					fieldValue[fieldValue.length] = newInput;
+	// 					form.setFieldValue(field.name, fieldValue);
+	// 				}
+	// 			} else {
+	// 				// add input to formik field value if not already added
+	// 				if (!fieldValue.find((e) => e === newInput)) {
+	// 					fieldValue[fieldValue.length] = newInput;
+	// 					form.setFieldValue(field.name, fieldValue);
+	// 				}
+	// 		}
+	// 	}
 
-	// Remove item/value from inserted field values
-	const removeItem = (key: number) => {
-		fieldValue.splice(key, 1);
-		form.setFieldValue(field.name, fieldValue);
-	};
+	// 		// reset inputValue
+	// 		setInputValue("");
+	// 	}
+	// }
+
+	// // Remove item/value from inserted field values
+	// const removeItem = (key: number) => {
+	// 	fieldValue.splice(key, 1);
+	// 	form.setFieldValue(field.name, fieldValue);
+	// };
 
 	return (
-		// Render editable field for multiple values depending on type of metadata field
-		// (types: see metadata.json retrieved from backend)
-		editMode ? (
-			<>
-				{fieldInfo.type === "mixed_text" && (
-					<EditMultiSelect
-						collection={fieldInfo.collection ? fieldInfo.collection : []}
-						field={field}
-						fieldValue={fieldValue}
-						inputValue={inputValue}
-						removeItem={removeItem}
-						handleChange={handleChange}
-						handleKeyDown={handleKeyDown}
-						handleBlur={submitValue}
-					/>
-				)}
-			</>
-		) : (
-			<ShowValue
-				setEditMode={setEditMode}
-				field={field}
-				form={form}
-				showCheck={showCheck}
-			/>
-		)
-	);
+		<Nyeh
+			fieldInfo={fieldInfo}
+			field={field}
+			form={form}
+			// text={getMetadataCollectionFieldName(fieldInfo, field, t)}
+		/>
+	)
+
+	// return (
+	// 	// Render editable field for multiple values depending on type of metadata field
+	// 	// (types: see metadata.json retrieved from backend)
+	// 	editMode ? (
+	// 		<>
+	// 			{fieldInfo.type === "mixed_text" && (
+	// 				<EditMultiSelect
+	// 					collection={fieldInfo.collection ? fieldInfo.collection : []}
+	// 					field={field}
+	// 					fieldValue={fieldValue}
+	// 					inputValue={inputValue}
+	// 					removeItem={removeItem}
+	// 					handleChange={handleChange}
+	// 					handleKeyDown={handleKeyDown}
+	// 					handleBlur={submitValue}
+	// 				/>
+	// 			)}
+	// 		</>
+	// 	) : (
+	// 		<ShowValue
+	// 			setEditMode={setEditMode}
+	// 			field={field}
+	// 			form={form}
+	// 			showCheck={showCheck}
+	// 		/>
+	// 	)
+	// );
 };
 
 // Renders multi select
@@ -229,4 +240,179 @@ const ShowValue = ({
 	);
 };
 
+const Nyeh = ({
+	fieldInfo,
+	field,
+	form,
+	showCheck = false,
+}: {
+	fieldInfo: MetadataField
+	field: FieldProps["field"]
+	form: FieldProps["form"]
+	showCheck?: boolean,
+}) => {
+	const { t } = useTranslation();
+
+	const editableRef = useRef<HTMLInputElement>(null);
+	const [focused, setFocused] = useState(false);
+	const onFocus = () => { console.log("onFocus"); setFocused(true); }
+	const onBlur = () => { console.log("onBlur"); setFocused(false); }
+
+
+
+	const [inputValue, setInputValue] = useState("");
+
+	let fieldValue = [...field.value];
+	const onlyCollectionValues = false;
+
+	// Handle change of value user currently types in
+	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const itemValue = e.target.value;
+		setInputValue(itemValue);
+	};
+
+	const handleKeyDown = (event: React.KeyboardEvent) => {
+		// Check if pressed key is Enter
+		if (event.keyCode === 13 && inputValue !== "") {
+			event.preventDefault();
+
+			submitValue();
+		}
+	};
+
+	const submitValue = (alternativeInput?: string) => {
+
+		let newInputValue = inputValue
+		if (alternativeInput) {
+			newInputValue = alternativeInput
+		}
+
+		if (newInputValue !== "") {
+			let splitArray = [];
+			if (fieldInfo.delimiter) {
+				splitArray = newInputValue.split(fieldInfo.delimiter).map(item => item.trim()).filter(Boolean);
+			} else {
+				splitArray = [newInputValue];
+			}
+
+			for (const newInput of splitArray) {
+				// Flag if only values of collection are allowed or any value
+				if (onlyCollectionValues) {
+					// add input to formik field value if not already added and input in collection of possible values
+					if (
+						!fieldValue.find((e) => e === newInput) &&
+						fieldInfo.collection?.find((e) => e.value === newInput)
+					) {
+						fieldValue[fieldValue.length] = newInput;
+						form.setFieldValue(field.name, fieldValue);
+					}
+				} else {
+					// add input to formik field value if not already added
+					if (!fieldValue.find((e) => e === newInput)) {
+						fieldValue[fieldValue.length] = newInput;
+						form.setFieldValue(field.name, fieldValue);
+					}
+			}
+		}
+
+			// reset inputValue
+			setInputValue("");
+		}
+	}
+
+	// Remove item/value from inserted field values
+	const removeItem = (key: number) => {
+		console.log("I try")
+		fieldValue.splice(key, 1);
+		form.setFieldValue(field.name, fieldValue);
+		console.log("I tried")
+	};
+
+
+	return (
+		<div
+			onClick={() => editableRef.current?.focus()}
+			style={{display: "flex",
+				// flexDirection: focused ? "column" : "row",
+				justifyContent: "space-between"}}
+		>
+
+			{/* Render array as text */}
+			{!focused &&
+			field.value instanceof Array &&
+			field.value.length !== 0 &&
+				<ul>
+					{field.value.map((item, key) => (
+						<li key={key}>
+							<span>{item}</span>
+						</li>
+					))}
+				</ul>
+			}
+
+			{/* Render input */}
+			<div
+				style={{
+					width: focused ? "100%" : "0%",
+					// flexDirection: "column"
+				}}
+				onFocus={onFocus}
+				onBlur={onBlur}
+			>
+				<input
+					ref={editableRef}
+					type="text"
+					name={field.name}
+					value={inputValue}
+					className="single-value"
+					onKeyDown={(e) => handleKeyDown(e)}
+					onChange={(e) => handleChange(e)}
+					placeholder={t("EDITABLE.MULTI.PLACEHOLDER")}
+					list="data-list"
+					// onFocus={onFocus}
+					// onBlur={onBlur}
+					style={{width: focused ? "98%" : "0%"}}
+				/>
+
+				{/* Display possible options for values as some kind of dropdown */}
+				<datalist id="data-list">
+					{fieldInfo.collection?.map((item, key) => (
+						<option key={key}>{item.value as ReactNode}</option>
+					))}
+				</datalist>
+
+				{/* Render blue label for all values already in field array */}
+				{
+				field.value instanceof Array &&
+				field.value.length !== 0 &&
+				<div>
+					{field.value.map((item, key) => (
+						<span className="ng-multi-value" key={key}>
+							{item}
+							<button className="button-like-anchor" onClick={() => removeItem(key)}>
+								<i className="fa fa-times" />
+							</button>
+						</span>
+
+					))}
+				</div>
+				}
+			</div>
+
+
+
+			{/* Render checkmark and pencils */}
+			<div>
+				{!focused && showCheck && (
+					<i
+						className={cn("saved fa fa-check", {
+							active: JSON.stringify(form.initialValues[field.name] ?? []) !== JSON.stringify(field.value ?? []),
+						})}
+					/>
+				)}
+				{!focused && <i className="edit fa fa-pencil-square" />}
+			</div>
+		</div>
+	);
+}
 export default RenderMultiField;

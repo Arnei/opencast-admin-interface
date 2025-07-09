d5e7672abd1ce270474f8dad71df5ad39bf77fd9
Arnei <arnewilken@yahoo.de>
2025-07-09 14:34:43 +0200

    Fix ACL role dropdown width
    The role dropdown in the ACL tabs could extend over its
    column width, due to its own fixed width.
    This fixes that.

--- src/components/shared/modals/ResourceDetailsAccessPolicyTab.tsx
+++ src/components/shared/modals/ResourceDetailsAccessPolicyTab.tsx
@@ -564,6 +564,7 @@ export const AccessPolicyTable = <T extends AccessPolicyTabFormikProps>({
 																				user,
 																			)
 																		}
+																		customCSS={{ width: "100%" }}
 																	/>
 																) : (
 																	<p>{policy.role}</p>

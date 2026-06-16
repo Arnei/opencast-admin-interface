ab7ca7a76ba4158aecb608f50b5af720c87be216
Arnei <arnewilken@yahoo.de>
2026-06-16 15:25:28 +0200

    Make "Create User" dialog similar to other create dialogs
    Out of all "Create" dialogs, the one for users was working
    differently from the all the others. As I can see no reason why
    it should be like that, this patch
    brings the "Create New User" dialog in line with all the other
    create dialogs, for consistencies and usabilities sake.

--- src/components/users/partials/wizard/UserRolesTab.tsx
+++ src/components/users/partials/wizard/UserRolesTab.tsx
@@ -4,6 +4,7 @@ import SelectContainer from "../../../shared/wizard/SelectContainer";
 import { FormikProps } from "formik";
 import { NotificationComponent } from "../../../shared/Notifications";
 import ModalContent from "../../../shared/modals/ModalContent";
+import WizardNavigationButtons from "../../../shared/wizard/WizardNavigationButtons";
 
 /**
  * This component renders the role selection tab of the new user wizard and the user details modal
@@ -14,8 +15,12 @@ interface RequiredFormProps {
 
 const UserRolesTab = <T extends RequiredFormProps>({
 	formik,
+	nextPage,
+	previousPage,
 }: {
 	formik: FormikProps<T>
+	nextPage?: (values: T) => void, // For create modal
+	previousPage?: (values: T) => void, // For create modal
 }) => {
 	// roles that can be chosen by user
 	const [roles, setRoles] = useState<Role[]>([]);
@@ -35,31 +40,41 @@ const UserRolesTab = <T extends RequiredFormProps>({
 	}, []);
 
 	return (
-		<ModalContent>
-			{!formik.values.manageable && (
-				<NotificationComponent
-					notification={{
-						type: "warning",
-						message: "NOTIFICATIONS.USER_NOT_MANAGEABLE",
-						id: 0,
-					}}
-				/>
-			)}
-			<div className="form-container">
-				{/* Select container for roles*/}
-				{!loading && (
-					<SelectContainer
-						resource={{
-							searchable: true,
-							label: "USERS.USERS.DETAILS.ROLES",
-							items: roles,
+		<>
+			<ModalContent>
+				{!formik.values.manageable && (
+					<NotificationComponent
+						notification={{
+							type: "warning",
+							message: "NOTIFICATIONS.USER_NOT_MANAGEABLE",
+							id: 0,
 						}}
-						formikField="assignedRoles"
-						manageable={formik.values.manageable}
 					/>
 				)}
-			</div>
-		</ModalContent>
+				<div className="form-container">
+					{/* Select container for roles*/}
+					{!loading && (
+						<SelectContainer
+							resource={{
+								searchable: true,
+								label: "USERS.USERS.DETAILS.ROLES",
+								items: roles,
+							}}
+							formikField="assignedRoles"
+							manageable={formik.values.manageable}
+						/>
+					)}
+				</div>
+
+			</ModalContent>
+			{previousPage && nextPage &&
+				<WizardNavigationButtons
+					formik={formik}
+					nextPage={nextPage}
+					previousPage={previousPage}
+				/>
+			}
+		</>
 	);
 };
 

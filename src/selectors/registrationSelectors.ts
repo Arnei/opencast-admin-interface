ecace10a450fcc236f3727bea2323f4665b712e6
Greg Logan <gregorydlogan@gmail.com>
2026-07-23 09:14:36 -0600

    Combining the last of the old utils script into the slice, moving selectors around to match

--- src/selectors/registrationSelectors.ts
+++ src/selectors/registrationSelectors.ts
@@ -3,10 +3,17 @@ import { RootState } from "../store";
 /**
  * This file contains selectors regarding information about the registration status
  */
-export const getRegistrationLoaded = (state: RootState) => state.registration.loaded;
+export const getRegistrationLoaded = (state: RootState) => state.registration.registrationLoaded;
 // Are we registered at all
 export const getRegistration = (state: RootState) => state.registration.registration;
+
+export const getStatisticsLoaded = (state: RootState) => state.registration.statisticsLoaded;
+// Gather the system statistics reportable to the registration server
+export const getStatistics = (state: RootState) => state.registration.statistics;
+
 // Are we able to talk to register.opencast.org
-export const getAbleToRegister = (state: RootState) => state.registration.ableToRegister;
-// Does our registration match the latest ToU on the core
-export const getAgreedLatestToU = (state: RootState) => state.registration.agreedToToU;
+export const getAbleToRegister = (state: RootState) => state.registration.registrationLoaded &&
+                                                       state.registration.ableToRegister;
+export const getAgreedLatestToU = (state: RootState) => state.registration.registrationLoaded &&
+                                                        state.registration.registration != null && // this is correct because the *endpoint* returns a bare 'null' when the cluster isn't registered
+                                                        state.registration.registration.termsVersionAgreed == state.registration.latestToU;
--- src/slices/registrationSlice.ts
+++ src/slices/registrationSlice.ts
@@ -1,35 +1,105 @@
 import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
-import { WritableDraft } from "immer";
 import { createAppAsyncThunk } from "../createAsyncThunkWithTypes";
 
 export type Registration = {
-  agreedToPolicy: boolean,
-  registered: boolean,
+  contactMe: boolean,
+  systemType: string,
+  allowsStatistics: boolean,
+  allowsErrorReports: boolean,
+  organisationName: string,
+  departmentName: string,
+  country: string,
+  postalCode: string,
+  city: string,
+  firstName: string,
+  lastName: string,
+  street: string,
+  streetNo: string,
+  email: string,
   termsVersionAgreed: string,
-}
-
-export type RegistrationState = {
-  loaded: boolean,
-  registration: Registration | null,
-  latestToU: string,
-  ableToRegister: boolean,
-  agreedToToU: boolean,
-  error: boolean
+  agreedToPolicy: boolean,
+  dateModified: string,
+  dateCreated: string,
 };
 
-type StateUpdate = {
-  registration: Registration | null,
+// FIXME: This could be expanded to have all the fields, it's missing the nested host stuff
+export type Statistics = {
+  statistics: {
+    /* the linter doesn't like these, but those are the names.  Should we change them?
+    adopter_key: string,
+    statistics_key: string,
+    ca_count: number,
+    event_count: number,
+    job_count: number,
+    series_count: number,
+    tenant_count: number,
+    total_minutes: number,
+    user_count: number, */
+    version: string,
+  }
+};
+
+export type RegistrationState = {
+  registrationLoaded: boolean,
+  registration: Registration,
+  statisticsLoaded: boolean,
+  statistics: Statistics,
   latestToU: string,
+  ableToRegister: boolean,
+  agreedToPolicy: boolean,
+  error: boolean
 };
 
 // Initial state of health status in redux store
 const initialState: RegistrationState = {
-  loaded: false,
-  registration: null,
+  registrationLoaded: false,
+  registration: {
+    contactMe: false,
+    systemType: "",
+    allowsStatistics: false,
+    allowsErrorReports: false,
+    organisationName: "",
+    departmentName: "",
+    country: "",
+    postalCode: "",
+    city: "",
+    firstName: "",
+    lastName: "",
+    street: "",
+    streetNo: "",
+    email: "",
+    termsVersionAgreed: "",
+    agreedToPolicy: false,
+    dateModified: "",
+    dateCreated: "",
+  },
+  statisticsLoaded: false,
+  statistics: {
+    statistics: {
+      /* the linter doesn't like these, but those are the names.  Should we change them?
+      adopter_key: "",
+      statistics_key: "string",
+      ca_count: -1,
+      event_count: -1,
+      job_count: -1,
+      series_count: -1,
+      tenant_count: -1,
+      total_minutes: -1,
+      user_count: -1,*/
+      version: "",
+    },
+    /* There are other keys here, like so
+    tobira: {
+      insert arbitrary stats data
+    },
+    But these, and other keys, are not always present and depend *entirely* on local config
+    Thus, we omit them here
+    */
+  },
   latestToU: "uninitialized",
   ableToRegister: false,
-  agreedToToU: false,
+  agreedToPolicy: false,
   error: false,
 };
 
@@ -51,6 +121,54 @@ export const fetchIsUpToDate = createAppAsyncThunk("registration/isUpToDate", as
   return res.data;
 });
 
+// This is whether the statistics used in the registration modal.
+export const fetchStatistics = createAppAsyncThunk("registration/statistics", async () => {
+  const res = await axios.get<Statistics>("/admin-ng/adopter/statistics");
+  return res.data;
+});
+
+
+  // post request for adopter information
+  export const postAdopterRegistration = async (
+    values: Registration,
+  ) => {
+    // build body
+    const body = new URLSearchParams();
+    body.append("contactMe", values.contactMe.toString());
+    body.append("systemType", values.systemType);
+    body.append("allowsStatistics", values.allowsStatistics.toString());
+    body.append("allowsErrorReports", values.allowsErrorReports.toString());
+    body.append("organisationName", values.organisationName);
+    body.append("departmentName", values.departmentName);
+    body.append("country", values.country);
+    body.append("postalCode", values.postalCode);
+    body.append("city", values.city);
+    body.append("firstName", values.firstName);
+    body.append("lastName", values.lastName);
+    body.append("street", values.street);
+    body.append("streetNo", values.streetNo);
+    body.append("email", values.email);
+    body.append("agreedToPolicy", values.agreedToPolicy.toString());
+    body.append("registered", "true");
+
+    // save adopter information and return next state
+    await axios.post("/admin-ng/adopter/registration", body, {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    });
+  };
+
+// delete adopter information
+export const deleteAdopterRegistration = async () => {
+  // delete adopter information
+  await axios.delete("/admin-ng/adopter/registration", {
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+  });
+};
+
 const registrationSlice = createSlice({
   name: "registration",
   initialState,
@@ -62,38 +180,27 @@ const registrationSlice = createSlice({
         Registration
       >) => {
         state.registration = action.payload;
-        const updatedState = {
-          registration: state.registration,
-          latestToU: state.latestToU,
-        };
-        state.agreedToToU = agreedLatestTerms(state, updatedState);
-        state.loaded = true;
+        state.registrationLoaded = true;
       })
       .addCase(fetchLatestToU.fulfilled, (state, action: PayloadAction<
         string
       >) => {
         state.latestToU = action.payload;
-        const updatedState = {
-          registration: state.registration,
-          latestToU: state.latestToU,
-        };
-        state.agreedToToU = agreedLatestTerms(state, updatedState);
       })
       .addCase(fetchIsUpToDate.fulfilled, (state, action: PayloadAction<
         boolean
       >) => {
         // This is true if the core can talk to https://register.opencast.org/, false otherwise
         state.ableToRegister = action.payload;
+      })
+      .addCase(fetchStatistics.fulfilled, (state, action: PayloadAction<
+        Statistics
+      >) => {
+        state.statistics = action.payload;
+        state.statisticsLoaded = true;
       });
   },
 });
-
-const agreedLatestTerms = (_state: WritableDraft<RegistrationState>, updatedState: StateUpdate) => {
-  if (null != updatedState.registration && "uninitialized" != updatedState.latestToU) {
-    return updatedState.registration.termsVersionAgreed === updatedState.latestToU;
-  }
-  return false;
-};
 
 // Export the slice reducer as the default export
 export default registrationSlice.reducer;
